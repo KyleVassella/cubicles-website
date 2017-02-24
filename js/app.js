a3c22108a218950b40ca340d9bcb1207701f2874
Kyle Vassella <kylevassella@gmail.com>
2017-02-23 21:07:29 -0600

    Cubicles demo website.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+		  $('.reviewSlides').slick({
+           arrows: true,
+           dots: true,
+           infinite: true,
+           speed: 350,
+           fade: false,
+           cssEase: 'linear',
+           autoplay: true,
+           autoplaySpeed: 5000
+           // appendArrows: $('.arrowAttach')
+         });
